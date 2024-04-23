149db9ab67252e1ccb39ddeae393bc50f1c00a8d
raghav patel <patelraghav014@gmail.com>
2024-04-23 17:27:42 +0530

    upadte

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def ask():
         return render_template('index.html')  # Example: Render a form for asking questions
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
